4a44c77270240b1cddf309489b9607e88c0b0c25
Jakey-Jakey <maarij.ha@gmail.com>
2025-03-30 07:23:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -393,7 +393,7 @@ def index():
                 is_processing_story = True
             active_task_id_for_template = task_to_check # Use the ID found
         else:
-            app.logger.warning(f"Task {task_to_check} found with unexpected state '{task_state}'. Treating as error and popping.")
+             app.logger.warning(f"Task {task_to_check} found with unexpected state '{task_state}'. Treating as error and popping.")
             results = get_task_result(task_to_check)
             delete_task_result(task_to_check)
             if not results:
